f802af3b860d61f10570496158a1163452220876
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-10-05 12:03:28 -0500

    Tipos de datos numéricos en Go: enteros, flotantes

--- main.go
+++ main.go
@@ -8,16 +8,25 @@ const testConst = "Test"
 func main() {
 	name, lastname := getCompleteName()
 	fmt.Printf(helloWorld, name, lastname)
+
 	var firstNumber int
 	var secondNumber int
+
 	fmt.Print("Ingresa un numero: ")
 	fmt.Scanf("%d", &firstNumber)
 	fmt.Print("Ingresa otro numero: ")
 	fmt.Scanf("%d", &secondNumber)
+
 	var suma = suma(firstNumber, secondNumber)
 	fmt.Printf("El resultado de la suma es %d \n", suma)
 	var resta = resta(firstNumber, secondNumber)
 	fmt.Printf("El resultado de la resta es %d \n", resta)
+
+	a, b, c := getVariables()
+	fmt.Println(a, b, c)
+
+	f32, f64 := getFloat()
+	fmt.Println(f32, f64)
 }
 
 func getCompleteName() (string, string) {
@@ -29,6 +38,13 @@ func getCompleteName() (string, string) {
 	fmt.Print("Ingresa tu Apellido: ")
 	fmt.Scanf("%s", &lastname)
 	return name, lastname
+}
+func getVariables() (int, int32, int64) {
+	return 1, 241700000, 1919191181818181
+}
+
+func getFloat() (float32, float64) {
+	return float32(0.1), float64(float32(0.1))
 }
 
 func suma(a int, b int) int {
